1e8c45b23f98529469419ae9e9aeb4ea5c1d459c
Reece Russell <me@reece-russell.co.uk>
2021-01-24 16:08:25 +0000

    added client mocks

---
+++ client/mock/mock.go
@@ -0,0 +1,4 @@
+//go:generate mockgen -package=mock -source=../users/client.go -destination=users/client.go
+//go:generate mockgen -package=mock -source=../auth/client.go -destination=auth/client.go
+
+package mock
